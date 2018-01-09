4e47630e5168497c9f3a5c3111c08e62b46a6f4f
Gura-San <gura.san@gmail.com>
2018-01-09 00:09:47 -0500

    connected the API to express

--- controllers/gameCardController.js
+++ controllers/gameCardController.js
@@ -1,15 +1,39 @@
-const Router      = require('express').Router()
-const Card        = require('../db/schema')
+const Router = require("express").Router();
+const Card = require("../db/schema");
+const igdb = require("igdb-api-node").default;
 
 //================== API Variables ==================
-// const client  = require('../keys/keys').client
-// const url         = require('../keys/keys').url
+require("dotenv").config();
+const client = igdb(process.env.MASHAPEKEY);
 //===================================================
 
+Router.get("/", (req, res) => {
+  res.render("game-card");
+});
 
-Router.get('/', (req, res) => {
-  res.render('game-card')
-  console.log(url)
-})
+Router.get("/:name", (req, res) => {
+  client;
+  client
+    .games(
+      {
+        filters: {
+          "release_dates.date-gt": "2010-12-31",
+          "release_dates.date-lt": "2017-01-01"
+        },
+        limit: 5,
+        offset: 0,
+        order: "release_dates.date:desc",
+        search: "Doom"
+      },
+      ["name", "release_dates.date", "rating", "hypes", "cover"]
+    )
+    .then(res => {
+      // response.body contains the parsed JSON response to this query
+      console.log(res.body);
+    })
+    .catch(error => {
+      throw error;
+    });
+});
 
-module.exports = Router
+module.exports = Router;
