8bddd6e567b23a1807285366f57e331619b6d247
Gura-San <gura.san@gmail.com>
2018-02-11 21:02:23 -0500

    tweaked the connection file, deleted depricated code

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,11 @@
 const mongoose = require('mongoose')
 const URL = 'mongodb://localhost/gameboard'
 
-// if (process.env.NODE_ENV == "production") {
-//   mongoose.connect(process.env.MLAB_URL)
-// } else {
-//   mongoose.connect(URI); 
-// }
-
-// after you're done uncomment ↑↑↑ and comment ↓↓↓
-
-mongoose.connect(URL, { useMongoClient: true })
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect(URL, { useMongoClient: true })
+}
 
 mongoose.Promise = Promise
 
